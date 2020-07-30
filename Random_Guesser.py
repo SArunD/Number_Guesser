4f9b35604b181372e27e67ae5892afa9af78fe01
SArunD <67386158+SArunD@users.noreply.github.com>
2020-07-30 17:07:04 -0400

    Update and rename Number_Guesser.py to Random_Guesser.py

--- Random_Guesser.py
+++ Random_Guesser.py
@@ -6,6 +6,7 @@ user_guess = int(input('Enter any number: '))
 # Instantiate global variables
 guess_lst = []
 try_count = 1
+
 # Length of user input
 ug_length = len(str(user_guess))
 
@@ -13,23 +14,28 @@ ug_length = len(str(user_guess))
 while True:
     # Empty list for adding random integer with length ug_length
     num_lst = []
-    # If ug_length > 1, the first number of my guess is != 0
+    
+    # If user input is > a digit, then the first number of my guess is != 0
     if ug_length > 1:
         num_lst.append(str(random.randint(1,9)))
     else:
         num_lst.append(str(random.randint(0,9)))
+    
     # Add random integers to num_lst to match ug_length
     for i in range(ug_length-1):
         num_lst.append(str(random.randint(0, 9)))
+    
     # Join all the numbers in num_lst and convert to int
     new_num = int(''.join(num_lst))
-    # Add new_num to guess_lst if not already guessed, else continue
+    
+    # Add new_num to guess_lst if not already guessed, else restart iteration
     if new_num not in guess_lst:
         guess_lst.append(new_num)
         print('Try {}: {}'.format(try_count, new_num))
         try_count += 1
     else:
         continue
+    
     # If the new_num is user input, print # of tries and break
     if new_num == user_guess:
         print('I guessed your number in {} tries'.format(try_count))
